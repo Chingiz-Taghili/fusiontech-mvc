15172f9490a0d42238f77f4bc97dcc6883ce8a87
Chingiz <cingiztagili@gmail.com>
2024-09-03 01:18:59 +0400

    initial

---
+++ src/main/java/com/multishop/fusiontech/FusiontechApplication.java
@@ -0,0 +1,13 @@
+package com.multishop.fusiontech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusiontechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusiontechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multishop/fusiontech/FusiontechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multishop.fusiontech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusiontechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
